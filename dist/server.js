513dcea39f686ba96785db424a55bc1543c843a3
Md Sakibul Islam <sakibul.islam0808@gmail.com>
2024-12-21 18:41:33 +0600

    refactor: restore server variable and enhance error handling for unhandled rejections and exceptions

--- dist/server.js
+++ dist/server.js
@@ -12,39 +12,37 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-/* eslint-disable no-console */
-// import { Server } from 'http';
 const mongoose_1 = __importDefault(require("mongoose"));
 const app_1 = __importDefault(require("./app"));
 const config_1 = __importDefault(require("./app/config"));
 const database_url = config_1.default.database_url;
 const port = config_1.default.port;
-// let server: Server;
+let server;
 function main() {
     return __awaiter(this, void 0, void 0, function* () {
         try {
             yield mongoose_1.default.connect(database_url);
             console.log('🛢️  Database connection successful ✅');
-            app_1.default.listen(port, () => {
+            server = app_1.default.listen(port, () => {
                 console.log(`🎯 Server listening on port: ${port}`);
             });
         }
         catch (err) {
-            console.error('🛢️  Failed to connect to database. ❌', err);
+            console.error('🛢️ Failed to connect to database. ❌', err);
         }
     });
 }
 main();
-// process.on('unhandledRejection', () => {
-//   console.log(`😈 unahandledRejection is detected , shutting down ...`);
-//   if (server) {
-//     server.close(() => {
-//       process.exit(1);
-//     });
-//   }
-//   process.exit(1);
-// });
-// process.on('uncaughtException', () => {
-//   console.log(`😈 uncaughtException is detected , shutting down ...`);
-//   process.exit(1);
-// });
+process.on('unhandledRejection', () => {
+    console.log(`😈 unahandledRejection is detected , shutting down ...`);
+    if (server) {
+        server.close(() => {
+            process.exit(1);
+        });
+    }
+    process.exit(1);
+});
+process.on('uncaughtException', () => {
+    console.log(`😈 uncaughtException is detected , shutting down ...`);
+    process.exit(1);
+});
